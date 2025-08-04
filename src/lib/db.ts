e1732eefa6940ab159ea1a79219fc396b53b730e
Ali Musthafa Kamal <alimusthafakamal@gmail.com>
2025-08-05 06:52:00 +0700

    refactor: menerapkan pola singleton

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,19 @@
+// File: lib/db.ts
+
 import { PrismaClient } from "@/generated/prisma/client";
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+// Menerapkan pola singleton untuk PrismaClient
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    // Opsi log bisa diaktifkan untuk debugging
+    log: ["query", "info", "warn", "error"],
+  });
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
